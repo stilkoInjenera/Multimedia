f571cb1608eb278102acc0a98d657c204b935732
Stelian Grozev <tanya_dzh@abv.bg>
2022-05-15 19:47:26 +0300

    success with fade out

--- JS/coverScript.js
+++ JS/coverScript.js
@@ -2,4 +2,6 @@ var cover = document.getElementById("cover");
 var carousel = document.getElementById("carouselExampleCaptions");
 function ActivatePlayer(){
     cover.classList.add("hidden");
+    carousel.style.visibility="visible";
+    LoadVideo(video1);
 }
