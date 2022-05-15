9954da28702f36a299508fa8c76d6dbd9765e454
Stelian Grozev <tanya_dzh@abv.bg>
2022-05-15 15:53:05 +0300

    video gets paused

--- videoScript.js
+++ videoScript.js
@@ -1,10 +1,14 @@
 
 // Get the video
-var video = document.getElementById("myVideo");
-
+var video1 = document.getElementById("myVideo1");
+var video2 = document.getElementById("myVideo2");
+var video3 = document.getElementById("myVideo3");
+var bt1 = document.getElementById("bt1");
+var bt2 = document.getElementById("bt2");
+var bt3 = document.getElementById("bt3");
 
 // Pause and play the video, and change the button text
-function pauseVideo() {
+function pauseVideo(video) {
   if (video.paused) {
     video.play();
   } else {
